535faf706bf63249b451317803bf006887acaa74
Steve Newman <115482991+NewmanJustice@users.noreply.github.com>
2025-07-03 11:18:50 +0100

    WIP

--- insert_admin.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO User (email, password_hash, name, role, notify_preferences, created_at, updated_at)
-VALUES ('admin@email.com', '$2b$10$wrelsLwmvmMtsnTcLst/reRxFMKW2KCImcf8tbAFuHge55/z1a/gO', 'admin', 'ADMIN', '{}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
